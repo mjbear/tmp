f5efbd95109ca95b472dfdab9c9882212dc1e7da
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-09 19:51:19 -0400

    Update blah.py

--- blah.py
+++ blah.py
@@ -1,6 +1,7 @@
 # silly test file for linting and formatting
 
-superlongstringname = f'blah'
+# superlongstringname = f'blah'
+superlongstringname = 'blah'
 
 if superlongstringname.startswith('b') and superlongstringname == 'blah'.lower():
     superlongstringname = superlongstringname.upper()
